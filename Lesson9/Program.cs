f3f034a22b836e9a4e36a43f339f3b5be02f388a
Oksana Gnyp <ognyp@softserveinc.com>
2022-01-28 20:07:30 +0200

    Lesson9

---
+++ Lesson9/Program.cs
@@ -0,0 +1,90 @@
+﻿using System;
+
+namespace Lesson9
+{
+    public delegate double Median(int first, int second, int third);
+
+    public delegate int CountMedian(params SomeIntDelegate[] delegatesArray);
+
+    public delegate int SomeIntDelegate();
+    
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Task2
+
+            Console.WriteLine("Enter firts operand:");
+            double firstNumber = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Enter second operand:");
+            double secondNumber = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Enter one of math operations: +, -, *, /");
+            string sign = Console.ReadLine();
+
+            switch (sign)
+            {
+                case "+":
+                    var AdditionResult = Calculator.Add(firstNumber, secondNumber);
+                    Console.WriteLine($"{firstNumber} + {secondNumber} = {AdditionResult}");
+                    break;
+                case "-":
+                    var SubstractionResult = Calculator.Sub(firstNumber, secondNumber);
+                    Console.WriteLine($"{firstNumber} - {secondNumber} = {SubstractionResult}");
+                    break;
+                case "*":
+                    var MultiplicationResult = Calculator.Mul(firstNumber, secondNumber);
+                    Console.WriteLine($"{firstNumber} * {secondNumber} = {MultiplicationResult}");
+                    break;
+                case "/":
+                    var DivisionResult = Calculator.Div(firstNumber, secondNumber);
+                    Console.WriteLine($"{firstNumber} / {secondNumber} = {DivisionResult}");
+                    break;
+                default:
+                    Console.WriteLine("Math operator is not correct!");
+                    break;
+            }
+
+            #endregion
+
+            #region Task5
+
+            Console.WriteLine(GetMedian(34, 50, 40));
+
+            #endregion
+
+            #region Task3
+
+            Random random = new Random();
+            SomeIntDelegate firstRandomNumber = new SomeIntDelegate(delegate () { return random.Next(5, 20); });
+            SomeIntDelegate secondRandom = GetRandomNumber;
+
+            CountMedian countMedian = new CountMedian((x)=> 
+                                                          {
+                                                             int? sum = 0;
+
+                                                             foreach (SomeIntDelegate item in x)
+                                                             {
+                                                                sum += item?.Invoke();
+                                                             }
+
+                                                             return (int)(object)sum/x.Length;
+
+                                                             });
+
+            Console.WriteLine(countMedian(firstRandomNumber,secondRandom));
+
+            #endregion
+
+            Console.ReadKey();
+        }
+
+        static Median GetMedian = new Median((a, b, c) => (a + b + c) / 3);
+
+        static int GetRandomNumber()
+        {
+            Random random = new Random();
+
+            return random.Next(30, 40);
+        }
+    }
+}
---
+++ Lesson9/Task2/Calculator.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Lesson9
+{
+    public delegate double MathOperations(double x, double y);
+
+    public class Calculator
+    {
+        public static MathOperations Add = new MathOperations((x, y) => x + y);
+
+        public static MathOperations Sub = new MathOperations((x, y) => x - y);
+
+        public static MathOperations Mul = new MathOperations((x, y) => x * y);
+
+        public static MathOperations Div = new MathOperations((x, y) =>
+                                                              {
+                                                                 if (y != 0)
+                                                                 {
+                                                                    return x / y;
+                                                                 }
+                                                                 else
+                                                                 { 
+                                                                    Console.WriteLine("You cannot divide by zero! The action " +
+                                                                                      "is rejected!");
+                                                                     return x;
+                                                                 }
+                                                               });
+    }
+}
