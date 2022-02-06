7954e1b893341e6ad8fc0b97631dffb7eaa433d4
Oksana Gnyp <ognyp@softserveinc.com>
2022-02-06 15:59:33 +0200

    Lesson13

---
+++ Lesson13/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Threading;
+
+namespace Lesson13
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Counter counter = new Counter();
+
+            Thread firstThread = new Thread(new ParameterizedThreadStart(counter.ShowCounter));
+            Thread secondThread = new Thread(new ParameterizedThreadStart(counter.ShowCounter));
+            Thread thirdThread = new Thread(new ParameterizedThreadStart(counter.ShowCounter));
+
+            firstThread.Start(1);
+            secondThread.Start(2);
+            thirdThread.Start(3);
+
+            Console.ReadKey();
+        } 
+    }
+}
---
+++ Lesson13/Task4/Counter.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Threading;
+
+namespace Lesson13
+{
+    public class Counter
+    {
+        Object block = new Object();
+       
+        public void ShowCounter(object counter)
+        {
+            int count = (int)counter;
+
+            lock (block)
+            {
+                Console.WriteLine("ShowCounter start");
+
+                for (int i = 0; i < 10; i++)
+                {
+                    Console.WriteLine(count);
+                    Thread.Sleep(1000);
+                    count++;
+                }
+
+                Console.WriteLine("ShowCounter stop");
+            }
+        }
+    }
+}
